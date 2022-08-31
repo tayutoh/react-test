c8ac5e45c9176f4f9758bc7714465c7092526cb0
tayutoh <33804162+tayutoh@users.noreply.github.com>
2022-09-01 03:09:04 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+const App = () => {
+  console.log("さいしょ");
+
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>おはようございました！</h1>
+      <ColorfulMessage color="blue">お元気ですか！？</ColorfulMessage>
+      <ColorfulMessage color="orange">お元気でした！</ColorfulMessage>
+      <button onClick={onClickCountUp}>CounUp!!!</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on / off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( ﾟдﾟ)</p>}
+    </>
+  );
+};
+
+export default App;
